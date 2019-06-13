8171ff5852ac47764bd21df9332e648d7666d6c1
Natalie McCroy <nataliemccroy@gmail.com>
2019-06-12 20:26:42 -0500

    added prop types on post container

--- instagram-app/src/components/PostContainer/PostContainer.js
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -1,3 +1,20 @@
 import React from 'react';
-import App from './App';
+import CommentSection from './components/CommentSection';
+import PropTypes from "prop-types";
 
+
+const PostContainer = props => {
+	return (
+		<div>
+			{props.data.map(post => (
+				<Post key={post.id} post={post} />	
+			))}
+		</div>
+  );
+};
+
+PostContainer.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.object).isRequired
+};
+
+export default PostContainer;
